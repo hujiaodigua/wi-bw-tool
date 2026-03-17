1b6ffb2d7f3167cd2c3cccd1c7e1cdcc8940e73c
yeeli <seven.yi.lee@gmail.com>
2026-03-17 11:30:13 +0800

    Add node detect feature
    Signed-off-by: yeeli <seven.yi.lee@gmail.com>

--- wi_bw_tool.c
+++ wi_bw_tool.c
@@ -1,33 +1,30 @@
-/*************************************************************************
-    > File Name: wi_bw_tool.c
-  > Author:
-  > Mail:
-  > Created Time: 2026年03月17日 星期二 10时41分22秒
- ************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define WEIGHTINESS 32
-#define MAX_NODES 256
+#define WEIGHTINESS	32
+#define MAX_NODES	256
+#define SYSFS_PATH	"/sys/kernel/mm/mempolicy/weighted_interleave"
 
 static unsigned int gcd(unsigned int a, unsigned int b)
 {
 	while (b) {
 		unsigned int t = a % b;
+
 		a = b;
 		b = t;
 	}
+
 	return a;
 }
 
 static unsigned int gcd_array(unsigned int *arr, int n)
 {
 	unsigned int g = arr[0];
+	int i;
 
-	for (int i = 1; i < n; i++)
+	for (i = 1; i < n; i++)
 		g = gcd(g, arr[i]);
 
 	return g;
@@ -39,7 +36,7 @@ static int write_weight(int node, unsigned int weight)
 	FILE *f;
 
 	snprintf(path, sizeof(path),
-		"/sys/kernel/mm/mempolicy/weighted_interleave/node%d", node);
+		 SYSFS_PATH "/node%d", node);
 
 	f = fopen(path, "w");
 	if (!f) {
@@ -53,60 +50,102 @@ static int write_weight(int node, unsigned int weight)
 	return 0;
 }
 
-int main(int argc, char **argv)
+static int detect_nodes(void)
+{
+	int count = 0;
+	int i;
+	char path[256];
+
+	for (i = 0; i < MAX_NODES; i++) {
+		snprintf(path, sizeof(path),
+			 SYSFS_PATH "/node%d", i);
+
+		if (access(path, F_OK) == 0)
+			count++;
+		else
+			break;
+	}
+
+	return count;
+}
+
+int main(void)
 {
 	unsigned long bw[MAX_NODES];
 	unsigned int weight[MAX_NODES];
 	unsigned long total = 0;
-	int nodes = argc - 1;
+	int nodes;
+	int i;
 
-	if (nodes <= 0) {
-		printf("usage: %s bw0 bw1 bw2 ...\n", argv[0]);
+	if (access(SYSFS_PATH, F_OK) != 0) {
+		printf("Error: %s not found\n", SYSFS_PATH);
+		printf("Please enable CONFIG_NUMA and weighted interleave in kernel.\n");
 		return 1;
 	}
 
-	for (int i = 0; i < nodes; i++) {
-		bw[i] = strtoul(argv[i + 1], NULL, 10);
+	nodes = detect_nodes();
+
+	if (nodes <= 0) {
+		printf("No nodeX entries found under %s\n", SYSFS_PATH);
+		printf("Kernel may not support weighted interleave.\n");
+		return 1;
+	}
+
+	printf("Detected %d nodes\n\n", nodes);
+
+	for (i = 0; i < nodes; i++) {
+		printf("Enter bandwidth for node%d (MB/s): ", i);
+		fflush(stdout);
+
+		if (scanf("%lu", &bw[i]) != 1) {
+			printf("Invalid input\n");
+			return 1;
+		}
+
 		total += bw[i];
 	}
 
 	if (!total) {
-		printf("total bandwidth is zero\n");
+		printf("Total bandwidth is zero\n");
 		return 1;
 	}
 
-	for (int i = 0; i < nodes; i++) {
+	for (i = 0; i < nodes; i++) {
 		weight[i] = (WEIGHTINESS * bw[i]) / total;
 		if (weight[i] == 0)
 			weight[i] = 1;
 	}
 
-	unsigned int g = gcd_array(weight, nodes);
+	{
+		unsigned int g = gcd_array(weight, nodes);
 
-	for (int i = 0; i < nodes; i++)
-		weight[i] /= g;
+		for (i = 0; i < nodes; i++)
+			weight[i] /= g;
+	}
 
-	printf("Calculated weights:\n");
+	printf("\nCalculated weights:\n");
 
-	for (int i = 0; i < nodes; i++)
-		printf("node%d: bw=%lu -> weight=%u\n", i, bw[i], weight[i]);
+	for (i = 0; i < nodes; i++)
+		printf("node%d: bw=%lu -> weight=%u\n",
+		       i, bw[i], weight[i]);
 
 	printf("\nWrite these weights to sysfs? [y/N]: ");
 	fflush(stdout);
 
+	/* 清理 stdin 中残留换行 */
+	while (getchar() != '\n')
+		;
+
 	int c = getchar();
 
-	/* 清理输入缓冲区 */
-	while (getchar() != '\n' && !feof(stdin));
-
 	if (c != 'y' && c != 'Y') {
-		printf("Aborted. Not writing to sysfs.\n");
+		printf("Aborted.\n");
 		return 0;
 	}
 
 	printf("\nWriting sysfs:\n");
 
-	for (int i = 0; i < nodes; i++)
+	for (i = 0; i < nodes; i++)
 		if(!write_weight(i, weight[i]))
 			printf("node%d: weight=%u written successfully\n", i, weight[i]);
 
